6ffeee34236234f980404c79fa2dc376fc04e6f9
AlexHung1406 <truongvanhung14062005@gmail.com>
2024-09-10 20:20:00 +0700

    Update Assigment10.java

--- src/assigment10/Assigment10.java
+++ src/assigment10/Assigment10.java
@@ -2,19 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package assigment10;
+package javaapplication22;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author Admin
  */
-public class Assigment10 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+        
+        System.out.println("Enter the number of books:");
+        int size = sc.nextInt();
+        sc.nextLine(); // Consume the newline
+
+        for (int i = 0; i < size; i++) {
+            Book book = new Book();
+            System.out.println("Enter book: " + (i+1));
+            book.inputBook();
+            books.add(book);
+        }
+
+        for (Book book : books) {
+            int i = 0;
+            System.out.println("book: " + (i+1));
+            book.displayBook();
+            i++;
+        }
+        
+        sc.close();
     }
     
 }
+
