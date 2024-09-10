0eefd601d5de0c62cc40a5851684ce83c46c464a
AlexHung1406 <truongvanhung14062005@gmail.com>
2024-09-10 20:17:59 +0700

    m

---
+++ src/assigment10/Assigment10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assigment10;
+
+/**
+ *
+ * @author Admin
+ */
+public class Assigment10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
